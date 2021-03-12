61d165ad85163e605f6e64c3f104dc14f63938ea
Brandon Le <greenunknown0@gmail.com>
2021-03-11 19:20:07 -0800

    Working on getting random anime by adding random step and seed.

--- random-anime.elm
+++ random-anime.elm
@@ -32,16 +32,29 @@ main =
 -- MODEL
 
 
-type Model
-  = Failure
-  | Loading
-  | Success String
+-- type Model
+--   = Failure
+--   | Loading
+--   | Success String
 
+type Status
+  = Failure
+    | Loading
+    | Success String
+
+type alias Model = 
+  { status : Status
+  , seed : Random.Seed
+  }
+
+
+-- init : () -> (Model, Cmd Msg)
+-- init _ =
+--   (Loading, gotRandomAnime)
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Loading, gotRandomAnime)
-
+  ({status = Loading, seed = Random.initialSeed 0}, gotRandomAnime)
 
 
 -- UPDATE
@@ -113,20 +126,37 @@ viewGif model =
 -- randomAnime = Random.int 1 10509 -- 10509 is an experimental upper limit of the number of anime
 -- seed0 = Random.initialSeed 42
 
+-- Random help from Augustin82 and wolfadex: https://discourse.elm-lang.org/t/convert-random-int-to-string-for-use-in-url-builder/7081/3
 roll : Random.Generator Int
 roll = 
   Random.int 1 6
 
+randomIntToString : Random.Generator Int -> String --Random.Generator String
+randomIntToString randomInt =
+    Random.map String.fromInt randomInt
+
+-- gotRandomAnime : Cmd Msg
+-- gotRandomAnime =
+--   Http.get
+--     { url = crossOrigin "https://api.jikan.moe/v3/anime/" [Random.generate (randomIntToString roll)] []
+--     , expect = Http.expectJson GotImg animeDecoder
+--     }
+
 gotRandomAnime : Cmd Msg
 gotRandomAnime =
-  Http.get
-    { url = crossOrigin "https://api.jikan.moe/v3/anime/" [roll] []
-    , expect = Http.expectJson GotImg animeDecoder
-    }
+  let
+    ( randStr, nextSeed ) =
+        Random.step (randomIntToString roll) model --model.seed
+  in
+  ( { model | seed = nextSeed }
+  , Http.get
+      { url = crossOrigin "https://api.jikan.moe/v3/anime/" [randStr] []
+      , expect = Http.expectJson GotImg animeDecoder
+      }
+  )
 
 
 animeDecoder : Decoder String
 animeDecoder =
-  -- field "data" (field "image_url" string)
   field "image_url" Json.Decode.string
   -- field "top" (field "image_url" string)
